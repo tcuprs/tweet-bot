1da32e0690cb82c0084a1543d011b3b46ff26879
Naoya Toyota <electrical.1847@gmail.com>
2021-01-18 14:03:49 +0900

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+# ローカル実行時にはここに環境変数をセット
+os.environ["YOUR_CONSUMER_KEY"] = ""
+os.environ["YOUR_CONSUMER_SECRET"] = ""
+os.environ["YOUR_TOKEN"] = ""
+os.environ["YOUR_TOKEN_SECRET"] = ""
